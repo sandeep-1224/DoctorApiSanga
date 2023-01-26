bb0b10fd7e9e5279ddb9700af8f0ecc753940fdd
sandeep-1224 <sandeepsharma8922@gmail.com>
2023-01-26 14:12:26 +0530

    fifth commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Doctor from './Doctor';
+// import Doctor from './Doctor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App />
     
-    <Doctor />
   </React.StrictMode>
 );
 
